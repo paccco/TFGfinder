acf814c01e488c9222ad1b3e1718932748a49d35
paccco <pacocarrascoconde@gmail.com>
2026-01-02 17:28:23 +0100

    Borrando backup

--- proyecto/deploy.sh
+++ proyecto/deploy.sh
@@ -4,4 +4,10 @@ doctl compute droplet create \
     --region fra1 \
     --enable-monitoring \
     --ssh-keys 52977094 \
+    --volumes 9efb5fe9-e7f2-11f0-8188-0a58ac12e8a3 \
     dockeronubuntu2204host
+    
+doctl compute firewall create \
+  --name "tfg-firewall" \
+  --inbound-rules "protocol:tcp,ports:3000,address:0.0.0.0/0 protocol:tcp,ports:3001,address:0.0.0.0/0 protocol:tcp,ports:22,address:0.0.0.0/0" \
+  --droplet-ids $(doctl compute droplet list --format ID --no-header)
